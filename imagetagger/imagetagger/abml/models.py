6bd7256834a30d7839b7b66475673ebd11927abb
jbargu <jure.grabnar@gmail.com>
2019-07-16 14:52:36 +0200

    Add skeleton migrations for ABMLExperiment

--- imagetagger/imagetagger/abml/models.py
+++ imagetagger/imagetagger/abml/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from imagetagger.images.models import ImageSet
 
 # Create your models here.
+
+class ABMLExperiment(models.Model):
+    RECOMMENDER = [
+        (0, 'baseline'),
+        (1, 'abml'),
+    ]
+
+    name = models.CharField(max_length=200)
+    image_set = models.ForeignKey(
+        ImageSet, on_delete=models.CASCADE)
+
+    recommender = models.IntegerField(choices=RECOMMENDER, default=0)
